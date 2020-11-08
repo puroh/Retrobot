68f8f8bd43ad3cdca1048128cab30b98095e2324
purohr <diego683@gmail.com>
2020-11-07 22:07:17 -0500

    Agregado el bot para telegram, agregado las funciones necesarias para que se intercambie texto entre el chat de telegram y las librerias de chatterbot. Aprendizaje de chatterbot en ingles(más completo) y español, responde preguntas generales pero no con mucha logica...

--- retrobot.py
+++ retrobot.py
@@ -1,11 +1,43 @@
 import credential # archivo con las contraseñas y keys privadas.
+
+#librerias de telegram
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import User
 
+#Librerias de chatterbot
+from chatterbot import ChatBot
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Entreanmiento del bot
+
+bot = ChatBot(
+    'Mr. Sypher',
+    #storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    #logic_adapters=[
+     #   'chatterbot.logic.MathematicalEvaluation',
+      #  'chatterbot.logic.TimeLogicAdapter',
+       # 'chatterbot.logic.BestMatch'
+    #],
+    database_uri='sqlite:///database.db'
+)
+trainer = ChatterBotCorpusTrainer(bot)
+
+trainer.train(
+    'chatterbot.corpus.english'
+    )  
+
+
+
+def convert_uppercase(update, context):
+    print("user: ", update.message.text)
+    bot_response = bot.get_response(update.message.text)
+    print(f'bot: {bot_response}')
+    chat_id = update.effective_chat.id
+    context.bot.send_message(chat_id = chat_id, text=str(bot_response ))
 
 def help(update, context):
     chat_id = update.effective_chat.id
-    context.bot.send_message(chat_id = chat_id, text = 'Bienvenido a @NOMBRE_BOT, con este bot podras consultar algunas caracteristicas.')
+    context.bot.send_message(chat_id = chat_id, text = 'Bienvenido a @rtrsofkabot, habla con este bot.')
     print('despues mensaje')
     
 def error(update, context):
@@ -17,6 +49,8 @@ def main():
     dp = updater.dispatcher
     dp.add_handler(CommandHandler('help',help))
     dp.add_error_handler(error)
+    dp.add_handler(MessageHandler(Filters.text & (~Filters.command), convert_uppercase))
+    escrito = MessageHandler(Filters.text & (~Filters.command), convert_uppercase)
     # Start the bot
     updater.start_polling()
     # Run the bot until you press Ctrl-C
